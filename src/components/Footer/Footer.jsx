5b8f61cefdf8b38353be38be4001974e7409bce9
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-06-30 10:00:00 +0100

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,78 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import Logoo from '../../assets/logoo.png'
+import './Footer.css';
+
+const title = "Company";
+const Resources = "Resources";
+const Product = "Product";
+const Support = "Contact";
+
+const addressList = [
+    { text: 'About Us' },
+    { text: 'Careers' },
+    { text: 'Press' },
+    { text: 'Ambassadors' },
+];
+
+const quickList = [
+    { text: 'All Products', link: '/shop' },
+    { text: 'Shop', link: '/shop' },
+    { text: 'Embed API', link: '/about' },
+    { text: 'Content Creators', link: '#' },
+    { text: 'Got a suggestion?', link: '/about' },
+];
+
+const tweetList = [
+    { text: 'FAQ', link: '#' },
+    { text: 'hire@micdee.com', link: '#' },
+    { text: 'WhatsApp ↗', link: '#' },
+];
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer className='footer'>
+            <div className='footer-container'>
+            <img src={Logoo} alt="Footer Image" className="footer-image" />
 
-export default Footer
+                <div className="footer-item">
+                    <div className="footer-content">
+                        <h4 className="footer-title">{title}</h4>
+                        <ul>
+                            {addressList.map((val, i) => (
+                                <li key={i}>{val.text}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="footer-item">
+                    <div className="footer-content">
+                        <h4 className="footer-title">{Product}</h4>
+                        <ul>
+                            {quickList.map((val, i) => (
+                                <li key={i}>
+                                    <a href={val.link}>{val.text}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="footer-item">
+                    <div className="footer-content">
+                        <h4 className="footer-title">{Support}</h4>
+                        <ul>
+                            {tweetList.map((val, i) => (
+                                <li key={i}>
+                                    <a href={val.link}>{val.text}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Header/Header';
 import Hero from '../Hero/Hero';
 import Card from '../Card/Card';
+import { Outlet } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import './Layout.css'; 
 import Services from '../Services/Services';
@@ -15,8 +16,6 @@ const Layout = () => {
       </div>
       <Card/>
       <Services/>
-      <div>
-      </div>
       <Footer />
     </>
   );
